b66b754066ca01b0133400e26d86554c845796c9
nopriterbang <novalpriya@gmail.com>
2024-04-26 14:42:25 +0700

    membuat looping dan perulangan

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -54,5 +54,24 @@ void addNode() {
 }
 
 bool searchNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
 
 }
