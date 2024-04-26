617a765727c1b8bc750f97ed6f2dd1f97335251f
nopriterbang <novalpriya@gmail.com>
2024-04-26 14:33:12 +0700

    membuat node previous and current

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -38,5 +38,14 @@ void addNode() {
 
 	while ((current != NULL) && (nim >= current->noMhs)) 
 	{
-	
+		if (nim == current->noMhs) 
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
 	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
